d08511ea07e609934d5df948e6796e2822e6e444
Bruno Grandi <bgrandi@outlook.com>
2025-02-25 17:32:02 -0300

    Subindo comentários

--- Curso Python/olamundo.py
+++ Curso Python/olamundo.py
@@ -1,3 +1,12 @@
+#Programa inicial "Hello World" mas com decisão "If/Else" para determinação de idioma
+
+"""
+Comentários
+de
+muitas
+linhas
+"""
+
 EN = str("Hello World!")
 PT = str("Ola mundo!")
 Idioma = input("Escolha a lingua para o programa (EN/PT)")
