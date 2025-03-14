3c02c2fce387783a6e21dac01ad54807d7a0a099
Bruno Grandi <bgrandi@outlook.com>
2025-03-14 12:43:30 -0300

    Subindo seção 5 e exercícios

---
+++ Curso Python/secao5/imc_calculo.py
@@ -0,0 +1,3 @@
+def imc_calc(peso,altura):
+    imc = peso/(altura ** 2)
+    return imc
---
+++ Curso Python/secao5/peso.py
@@ -0,0 +1,10 @@
+g_terra = 9.8
+g_lua = 1.6
+
+def  peso_lua(massa):
+    p_lua = g_lua * massa
+    return p_lua
+
+def peso_terra(massa):
+    p_terra = g_terra * massa
+    return p_terra
